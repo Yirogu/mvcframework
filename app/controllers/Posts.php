2e4531426a72fd8f8360e0764980962142e5c008
Michal Stachowski <stachudcs@gmail.com>
2019-09-16 22:20:56 +0200

    100% course

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -50,12 +50,63 @@ class Posts extends Controller {
             }
 
         } else {
-        $posts = $this->postModel->getPosts();
+//        $posts = $this->postModel->getPosts();
         $data = [
             'title'=> '',
             'body' => ''
         ];
         $this->view('posts/add',$data);
+        }
+    }
+    public function edit($id){
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            //Santitize POST array
+            $_POST = filter_input_array(INPUT_POST,FILTER_SANITIZE_STRING);
+            $data = [
+                'id' => $id,
+                'title'=> trim($_POST['title']),
+                'body' => trim($_POST['body']),
+                'user_id' => $_SESSION['user_id'],
+                'title_err' => '',
+                'body_err' => ''
+            ];
+            //Validate title
+            if (empty($data['title'])){
+                $data['title_err'] = 'please enter title';
+            }
+            if (empty($data['body'])){
+                $data['body_err'] = 'please enter body text' ;
+            }
+            if (empty($data['title_err']) && empty($data['body_err'])){
+//                Validated
+                if ($this->postModel->updatePost($data)){
+                    flash('post_message','Post Updated');
+                    redirect('posts');
+                } else{
+                    die('someting went wrong');
+                }
+
+            }else {
+                $this->view('posts/edit',$data);
+
+            }
+
+        } else {
+            //get existsing post from model
+            $post = $this->postModel->getPostById($id);
+
+            //Check for owner
+            if ($post->user_id != $_SESSION['user_id']){
+                redirect('posts');
+
+
+            }
+            $data = [
+                'id' => $id,
+                'title'=> $post->title,
+                'body' => $post->body
+            ];
+            $this->view('posts/edit',$data);
         }
     }
     public function show($id){
@@ -66,5 +117,25 @@ class Posts extends Controller {
             'user' => $user];
         $this->view('posts/show',$data);
 
+    }
+    public  function delete($id){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            //get existsing post from model
+            $post = $this->postModel->getPostById($id);
+
+            //Check for owner
+            if ($post->user_id != $_SESSION['user_id']){
+                redirect('posts');}
+            if ($this->postModel->deletePost($id)){
+                flash('post_message', 'Post Removed');
+                redirect('posts');
+            }
+        else{
+            die('something went wrong');
+        }
+
+        }else {
+            redirect('posts');
+        }
     }
 }
